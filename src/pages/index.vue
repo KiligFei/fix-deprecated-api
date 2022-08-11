909c08572cf2365781d3c3024e6d89a5a0b9953e
任锐 <renrui@renrui.local>
2022-08-11 14:02:43 +0800

    feat: :sparkles: init commin

---
+++ src/pages/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div class="testBtn">
+      <el-button>
+        Success
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<style lang='less' scoped>
+:deep(.testBtn){
+.el-button{
+    background-color: rgb(82, 183, 200);
+    width: 100px;
+  }
+}
+/deep/ .testBtn{
+  .el-button{
+    background-color: rgb(60, 35, 185);
+  }
+}
+::v-deep .testBtn{
+  .el-button{
+    background-color: rgb(120, 184, 120);
+  }
+}
+</style>
