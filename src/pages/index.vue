e38131e792249169e978ae9e4d689a66dc121eb4
renrui <wx_renr@novastar.tech>
2023-04-13 12:09:07 +0800

    feat: :sparkles: test vue-reuse-template

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
+import { DefineTemplate, ReuseTemplate } from 'vue-reuse-template';
+const dialogVisible = ref(false)
+const setDialogVisible = () => {
+  dialogVisible.value = !dialogVisible.value
+}
 </script>
 
 <template>
-  <div>
-    <div class="testBtn">
+  <DefineTemplate>
+    <div class="testBtn" @click="setDialogVisible">
       <el-button>
-        Success
+        {{ dialogVisible ? 'success' : 'error' }}
       </el-button>
     </div>
+  </DefineTemplate>
+  <div v-if="dialogVisible">
+    <ReuseTemplate />
+  </div>
+  <div v-else>
+    <ReuseTemplate />
   </div>
 </template>
 
 <style lang='less' scoped>
-:deep( .testBtn){
-  .el-button{
+:deep(.testBtn) {
+  .el-button {
     background-color: rgb(120, 184, 120);
   }
 }
